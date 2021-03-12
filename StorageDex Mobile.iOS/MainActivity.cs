5aba6a19d9a8204be2e34d15e292bdfeb15f685f
Vulcan19rl <69214138+Vulcan19rl@users.noreply.github.com>
2021-03-12 13:34:21 -0800

    Add files via upload

---
+++ StorageDex Mobile.iOS/MainActivity.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Plugin.Media;
+using FFImageLoading.Forms.Platform;
+using Rg.Plugins.Popup.Services;
+using Android.Support.V4.Content;
+using Android;
+using Plugin.CurrentActivity;
+using Xamarin.Essentials;
+using ZXing.Mobile;
+
+namespace StorageDex_Mobile.Droid
+{
+    [Activity(Label = "StorageDex Mobile", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        internal static MainActivity Instance { get; private set; }
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            await CrossMedia.Current.Initialize();
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+           
+
+            Instance = this;
+
+            CachedImageRenderer.Init(enableFastRenderer: true);
+
+            Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
+
+
+            //start ads
+            Android.Gms.Ads.MobileAds.Initialize(ApplicationContext, "ca-app-pub-6494346638793326~7521323299");
+
+            //start barcode proccesor
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+            MobileBarcodeScanner.Initialize(Application);
+
+            CrossCurrentActivity.Current.Activity = this;
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CachedImageRenderer.InitImageViewHandler();
+            base.OnCreate(savedInstanceState);
+            LoadApplication(new App());
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public static readonly int PickImageId = 1000;
+     
+
+        public TaskCompletionSource<Stream> PickImageTaskCompletionSource { set; get; }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            base.OnActivityResult(requestCode, resultCode, intent);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (intent != null))
+                {
+                    Android.Net.Uri uri = intent.Data;
+                    Stream stream = ContentResolver.OpenInputStream(uri);
+
+                    // Set the Stream as the completion of the Task
+                    PickImageTaskCompletionSource.SetResult(stream);
+                }
+                else
+                {
+                    PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
+
+        }
+
+        public override void OnBackPressed()
+        {
+            if (Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed))
+            {
+                PopupNavigation.Instance.PopAsync();
+            }
+            else
+            {
+
+
+            }
+        }
+
+   
+
+
+
+
+
+
+
+
+
+
+    }
+}
